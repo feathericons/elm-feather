a6a85c27ee6ced057d68ea050943b5b818ded3e0
Anatoliy <ac@ub.io>
2017-11-26 21:27:07 +0000

    Added example doc of customIcon

--- src/FeatherIcons.elm
+++ src/FeatherIcons.elm
@@ -291,6 +291,18 @@ type IconBuilder msg
 
 
 {-| Build custom svg icon
+
+    [ Svg.line [ x1 "21", y1 "10", x2 "3", y2 "10" ] []
+    , Svg.line [ x1 "21", y1 "6", x2 "3", y2 "6" ] []
+    , Svg.line [ x1 "21", y1 "14", x2 "3", y2 "14" ] []
+    , Svg.line [ x1 "21", y1 "18", x2 "3", y2 "18" ] []
+    ]
+        |> customIcon
+        |> withSize 2.1
+        |> withSizeUnit "em"
+        |> toHtml []
+
+Example output: <svg xmlns="http://www.w3.org/2000/svg" width="2.1em" height="2.1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="21" y1="10" x2="3" y2="10"></line><line x1="21" y1="6" x2="3" y2="6"></line><line x1="21" y1="14" x2="3" y2="14"></line><line x1="21" y1="18" x2="3" y2="18"></line></svg>
 -}
 customIcon : List (Svg msg) -> IconBuilder msg
 customIcon src =
@@ -352,8 +364,8 @@ toHtml attributes (IconBuilder { src, attrs }) =
 
         baseAttributes =
              [ fill "none"
-             , height strSize
-             , width strSize
+             , height <| strSize ++ attrs.sizeUnit
+             , width <| strSize ++ attrs.sizeUnit
              , stroke "currentColor"
              , strokeLinecap "round"
              , strokeLinejoin "round"
